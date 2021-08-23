134c5401c562dd1c10c4017b58bdbc62d492842c
elghaied <elghaied.eslam@gmail.com>
2021-08-23 11:45:10 +0200

    first build - connecting activity with java class

--- app/src/main/java/com/elghaied/android/forecast/MainActivity.java
+++ app/src/main/java/com/elghaied/android/forecast/MainActivity.java
@@ -18,11 +18,11 @@ import com.elghaied.android.forecast.databinding.ActivityMainBinding;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityMainBinding binding;
+
     private TextView mTextViewCityName;
     private TextView mTextViewCityDesc;
     private TextView mTextViewCityTemp;
@@ -31,6 +31,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mTextViewCityName =(TextView) findViewById(R.id.text_city_name);
+        mTextViewCityDesc =(TextView) findViewById(R.id.text_city_desc);
+        mTextViewCityTemp =(TextView) findViewById(R.id.text_city_temp);
+
+
+        mTextViewCityName.setText(R.string.city_name);
+        mTextViewCityDesc.setText(R.string.city_desc);
+        mTextViewCityTemp.setText(R.string.city_temp);
+        Toast.makeText(this, "your class is connected", Toast.LENGTH_SHORT).show();
 
     }
 
