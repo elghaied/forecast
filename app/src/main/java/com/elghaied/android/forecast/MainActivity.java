e0f613f6742473fc030d35a14cf1b29c621c919e
elghaied <elghaied.eslam@gmail.com>
2021-08-23 12:08:27 +0200

    adding buttons

--- app/src/main/java/com/elghaied/android/forecast/MainActivity.java
+++ app/src/main/java/com/elghaied/android/forecast/MainActivity.java
@@ -1,5 +1,8 @@
 package com.elghaied.android.forecast;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -17,6 +20,7 @@ import com.elghaied.android.forecast.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -26,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView mTextViewCityName;
     private TextView mTextViewCityDesc;
     private TextView mTextViewCityTemp;
+    private LinearLayout mLinearLayoutConnectionNotification;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,11 +40,19 @@ public class MainActivity extends AppCompatActivity {
         mTextViewCityDesc =(TextView) findViewById(R.id.text_city_desc);
         mTextViewCityTemp =(TextView) findViewById(R.id.text_city_temp);
 
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(
+                Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+            Toast.makeText(this, "you are connected", Toast.LENGTH_SHORT).show();
+            mLinearLayoutConnectionNotification.setVisibility(View.INVISIBLE);
+        } else {
+            mLinearLayoutConnectionNotification.setVisibility(View.VISIBLE);
+        }
 
         mTextViewCityName.setText(R.string.city_name);
         mTextViewCityDesc.setText(R.string.city_desc);
         mTextViewCityTemp.setText(R.string.city_temp);
-        Toast.makeText(this, "your class is connected", Toast.LENGTH_SHORT).show();
 
     }
 
